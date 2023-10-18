93f482484ad934c20a36710cbf8f9c0a4f967568
Odyn666 <lambertmorhen@wp.pl>
2023-10-18 13:47:42 +0200

    System.out.println(repository.findByCity("paris")); added to main app

--- src/main/java/pl/hajduk/footballerTask/App.java
+++ src/main/java/pl/hajduk/footballerTask/App.java
@@ -1,8 +1,8 @@
 package pl.hajduk.footballerTask;
 
-import pl.hajduk.footballerTask.repositories.FootballerRepository;
 import pl.hajduk.footballerTask.logic.FootballerDatabase;
 import pl.hajduk.footballerTask.model.Club;
+import pl.hajduk.footballerTask.repositories.FootballerRepository;
 
 import java.util.List;
 import java.util.Set;
@@ -10,24 +10,28 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
-        FootballerRepository repository=new FootballerDatabase();
-       // System.out.println(repository.findAll());
+        FootballerRepository repository = new FootballerDatabase();
+        // System.out.println(repository.findAll());
         System.out.println("******************");
         System.out.println(repository.findFootballersByClub("FC Barcelona"));
 
-        List<String>listClubs=repository.findAll().stream()
-                .flatMap(player->player.getPreviousClubs()
+        List<String> listClubs = repository.findAll().stream()
+                .flatMap(player -> player.getPreviousClubs()
                         .stream())
                 .map(Club::getClubName)
                 .distinct()
                 .toList();
 
-        Set<String>uniqueClubs=repository.findAll().stream()
-                        .flatMap(player->player.getPreviousClubs().stream())
+        Set<String> uniqueClubs = repository.findAll().stream()
+                .flatMap(player -> player.getPreviousClubs().stream())
                 .map(Club::getClubName)
                 .collect(Collectors.toSet());
-        System.out.println("liczba unikalnych klubów"+ listClubs.size());
+        System.out.println("liczba unikalnych klubów" + listClubs.size());
         System.out.println(uniqueClubs);
+
+// szukanie po miejsciu zamieszkania
+
+        System.out.println(repository.findByCity("paris"));
 
     }
 }
